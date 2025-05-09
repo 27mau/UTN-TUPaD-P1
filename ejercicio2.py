d8681df70f62a9f5ae7fe9fccaa403bfaa94f870
27mau <gaspar.mau27@gmail.com>
2025-05-09 11:44:34 -0300

    Agregando TP5

---
+++ ejercicio2.py
@@ -0,0 +1,8 @@
+# 2
+"""Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el 
+penúltimo. ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el 
+indexing con números negativos!"""
+
+mi_lista = ["pera", "manzana", "naranja", "limón", "banana"]
+
+print(mi_lista[-2]) # [-2] devuelve "limón", porque es el segundo elemento desde el final.
---
+++ ejercicio5.py
@@ -0,0 +1,13 @@
+# 5
+"""Analizar el siguiente programa y explicar con tus palabras qué es lo que realiza."""
+
+numeros = [8, 15, 3, 22, 7]
+
+numeros.remove(max(numeros))
+
+print(numeros)
+
+"""
+Tengo una lista de números y se usa la función max para buscar el valor máximo,
+y luego se usa la función remove para eliminarlo de la lista.
+"""
